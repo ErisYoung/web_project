13a10ae7da2727d6258caef806d92f9a6dcba248
toom1970 <toom1970@163.com>
2018-06-10 22:39:57 +0800

    change and add community

---
+++ js/CodingSearch.js
@@ -0,0 +1,8 @@
+    $(function (){
+        $("button.login").mouseover(function() {
+            $("button.login").css("background","#00893C");
+        });
+        $("button.login").mouseout(function() {
+            $("button.login").css("background","#27AE60");
+        });
+    });
