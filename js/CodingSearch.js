62bcce13d3ddf4283947e529d19e1b278d7aa3ab
toom1970 <toom1970@163.com>
2018-06-10 23:30:36 +0800

    add register

--- js/CodingSearch.js
+++ js/CodingSearch.js
@@ -5,4 +5,19 @@
         $("button.login").mouseout(function() {
             $("button.login").css("background","#27AE60");
         });
+
+        $("button.sign").mouseover(function() {
+            $("button.sign").css("background","#00893C");
+        });
+        $("button.sign").mouseout(function() {
+            $("button.sign").css("background","#27AE60");
+        });
+    });
+    $(function(){
+        $("a").mouseover(function() {
+            $(this).css("color","white");
+        });
+        $("a").mouseout(function() {
+            $(this).css("color","#dddddd");
+        });
     });
