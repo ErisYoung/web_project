365df3efe9b39739cc64bcfcda1a9d4805640670
jerry <jerry@example.com>
2018-06-18 00:13:50 +0800

    add the function of searching the questions in Internet

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,37 +1,53 @@
 $(function () {
-    //按钮单击时执行
-    $("#testAjax").click(function () {
-        //Ajax调用处理
-        $.ajax({
-            type: "POST",
-            url: "/home",
-            data: {
-                val: 1
-            },
-            success: function (data) {
-                console.log(data);
-                // var x="";
-                // data.forEach(dat => {
-                //     var st="<h1>"+dat.hha+"</h1><h2>"+dat.title+"</h2>";
-                //     x+=st;
-                // });
-                // $('#show').html(x);
-                bindHTML(data);
-                
-                // alert('success');
-            }
-        });
+    // test 板块
+    // $("#testAjax").click(function () {
+    //     //Ajax调用处理
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "/home",
+    //         data: {
+    //             val: 1
+    //         },
+    //         success: function (data) {
+    //             console.log(data);
+    //             // var x="";
+    //             // data.forEach(dat => {
+    //             //     var st="<h1>"+dat.hha+"</h1><h2>"+dat.title+"</h2>";
+    //             //     x+=st;
+    //             // });
+    //             // $('#show').html(x);
+    //             // bindHTML(data);
+    //             // alert('success');
+    //         }
+    //     });
+    // });
+    $("#search-button").on("click",function () {
+        var searchword=$("#searchbar").val();
 
+       $.ajax({
+           type:"POST",
+           url:"/home/search",
+           data:{
+            searchword:searchword
+           },
+           success:function(data){
+
+            //     $("#question").append(data[0].detail);
+            //    console.log('---------------------'+data);
+               bindHTML(data);
+           }
+       });
     });
+
 });
 
-function bindHTML(data) {
-    var str =  $('#newshow').text();
-    var st=  " <% if(data) { %>"+
-        "<% data.forEach(function(dat){  %><h1><%= dat.hha %></h1><% console.log(dat.hha+'-------------------') %><% }) %><% } %> "
+function bindHTML(datas) {
+    // var str =  $('#newshow').text();
+    var st=  '<% datas.forEach(function(data){  %><div class="question"><a href=<%= data.link %>><%= data.title %></a><div class="excerpt"><%= data.excerpt %></div></div><% }) %>';
     var result = ejs.render(st, {
-        data: data
+        datas: datas
     });
-    console.log(result);
-    $('#show').html(result);
+    // console.log(result+'------------------------');
+    $('#content').html(result);
+    
 }
